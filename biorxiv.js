52724a2219394b89e36114d5d2638b9c55e79631
judell <jon@jonudell.info>
2019-03-20 16:09:09 -0700

    initial

---
+++ biorxiv.js
@@ -0,0 +1,71 @@
+function delay(seconds) {
+  return new Promise((resolve) => setTimeout(resolve, seconds * 1000))
+}
+
+const groups = {
+  '6gPr7Xd3': 'Demo Peerage of Science',
+  'yr4Z4ve8': 'Demo EMBOJournal',
+}
+
+const groupCounts = {
+  '6gPr7Xd3': 0,
+  'yr4Z4ve8': 0,
+}
+
+
+async function run() {
+  await delay(.5)
+  const params = {
+    url: location.href
+  }
+  const data = await hlib.search(params)
+  const allAnnos = data[0]
+  const allReplies = data[1]
+  let groupAnnos = 0
+  let groupReplies = 0
+  allAnnos.concat(allReplies).forEach (a => {
+    if (a.group === '6gPr7Xd3' || a.group === 'yr4Z4ve8') {
+      const anno = hlib.parseAnnotation(a)
+      groupCounts[a.group]++
+      if (anno.isReply) {
+        groupReplies++
+      } else {
+        groupAnnos++
+      }
+    }     
+  })
+
+  function showReviews() {
+
+  }
+
+  const counterElement = hlib.getById('ccountx')
+  counterElement.innerHTML = `review groups: 2, annos: ${groupAnnos}, replies ${groupReplies}`
+
+  let reviewGroupsWithAnnotations = ''
+  Object.keys(groups).forEach ( group => {
+    if ( groupCounts[group]) {
+      reviewGroupsWithAnnotations += `
+        <div><a target="_hypothesisReviewGroup" href="https://hypothes.is/groups/${group}">${groups[group]}</a>`
+    }
+  })
+
+  const newToolHtml = `
+      <div class="pane-content">
+        <div class="minipanel-dialog-wrapper">
+          <div class="minipanel-dialog-link-link">
+            <a href="showReviews()" title="Reviews"><i class="icon-globe"></i> 
+              <span class="title">Reviews</span>
+            </a>
+          <div class="hypothesisReviewGroups">
+            ${reviewGroupsWithAnnotations} 
+          </div>
+        </div>  
+      </div>`
+
+  const existingToolElement = document.querySelector('.pane-biorxiv-cite-tool')
+
+  existingToolElement.innerHTML = newToolHtml
+}
+
+run()
